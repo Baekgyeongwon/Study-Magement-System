2254479bf133f39bb8e4645534cb1b5aaac017b9
Baek gyeong won <ruddnjs9060@naver.com>
2022-03-13 01:27:32 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Study");
+		System.out.println("2. Delete Study");
+		System.out.println("3. Edit Study");
+		System.out.println("4. View Study");
+		System.out.println("5. Exit");
+
+	}
+
+}
