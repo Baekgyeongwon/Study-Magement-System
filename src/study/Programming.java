d588bf9d9ff7d3332ccf256d82e04f381f1412c6
Baek gyeong won <ruddnjs9060@naver.com>
2022-05-02 11:11:05 +0900

    revision(assignment)

---
+++ src/study/Programming.java
@@ -0,0 +1,64 @@
+package study;
+
+import java.util.Scanner;
+
+public class Programming extends Study {
+	public Programming (StudyKind kind) {
+		super(kind);
+	}
+
+
+	public void getUserInput(Scanner input) {
+		System.out.print("studyCode: ");
+		int code = input.nextInt();
+		this.setCode(code);
+		
+		System.out.print("Subject name: ");
+		String subject = input.next();
+		this.setSubject(subject);
+		
+		System.out.print("Professor: ");
+		String prof = input.next();
+		this.setProf(prof);
+		
+		
+		char answer = 'x';
+		while(answer !='y' && answer !='Y' && answer !='n' && answer !='N') {
+			System.out.print("Is it offline class? (Y/N): ");
+			answer = input.next().charAt(0);
+			if(answer=='y' || answer == 'Y') {
+				System.out.print("Classroom: ");
+				String classroom =input.next();
+				this.setClassroom(classroom);
+				break;
+			}
+			
+			else if (answer == 'n' || answer == 'N') {
+				this.setClassroom("");
+				break;
+			}
+			else {
+			}
+		}
+	}
+	
+	public void printInfo() {
+		String skind = "none";
+		switch(this.kind) {
+		case Dynamics:
+			skind = "Dynamics";
+			break;
+		case Humanities:
+			skind = "Humanities";
+			break;
+		case Programming:
+			skind = "Programming";
+			break;
+		case Mathematics:
+			skind = "Mathematics";
+			break;
+		default:
+		}
+		System.out.println("kind: " +skind+ "code: "+code+" subject: "+subject+" prof: "+prof+" classroom: "+classroom);
+	}
+}
